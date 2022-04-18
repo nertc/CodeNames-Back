42e05e922883fb7d3c423b3e0172c836d982a8c6
nertc <davidtsiklauri7@gmail.com>
2022-04-18 16:15:20 +0400

    feat(wordlist): add word generator

---
+++ wordlist.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+const buffer = fs.readFileSync("wordlist.txt");
+const words = buffer.toString().split("\r\n");
+
+function getWords(numOfWords) {
+  const generatedWords = [];
+  const usedWords = {};
+  while (generatedWords.length < numOfWords) {
+    const wordId = Math.floor(Math.random() * words.length);
+    const word = words[wordId];
+    if (!usedWords[word]) {
+      usedWords[word] = true;
+      generatedWords.push(word);
+    }
+  }
+
+  return generatedWords;
+}
+
+module.exports = {
+  getWords,
+};
