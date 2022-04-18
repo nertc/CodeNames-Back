4dd1422fb3773a44c7c484c56e0c4c9ddad09623
nertc <davidtsiklauri7@gmail.com>
2022-04-18 16:15:40 +0400

    feat(team): add team generator

---
+++ teams.js
@@ -0,0 +1,17 @@
+function getTeams(numberOfTeams) {
+  const teams = [];
+  while (numberOfTeams.length) {
+    const index = Math.floor(Math.random() * numberOfTeams.length);
+    teams.push(numberOfTeams[index].team);
+    numberOfTeams[index].count--;
+    if (numberOfTeams[index].count <= 0) {
+      numberOfTeams[index] = numberOfTeams[numberOfTeams.length - 1];
+      numberOfTeams.pop();
+    }
+  }
+  return teams;
+}
+
+module.exports = {
+  getTeams,
+};
